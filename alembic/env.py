ba89e4934e0ec92883d416a71e918c6787f44adc
Vitalii Malkov <malkoff@inbox.ru>
2024-11-05 19:55:34 +0300

    After first review pep8

--- alembic/env.py
+++ alembic/env.py
@@ -18,8 +18,8 @@ config = context.config
 config.set_main_option('sqlalchemy.url', os.environ['DATABASE_URL'])
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+# if config.config_file_name is not None:
+#     fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
